7f68945636c405b226189f47bd8f2fe6fa27b89f
cshea892 <cshea892@students.bju.edu>
2018-05-22 03:36:35 -0400

    actually updated files, added ps file to script shortcut creation

---
+++ createShortcut.ps1
@@ -0,0 +1,6 @@
+﻿# Creates a shortcut into the shortcuts folder from the go location
+#These need to be replaced with paths relative to the user and the executables location
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("C:\Users\carte\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\todo.lnk")
+$Shortcut.TargetPath = "C:\Users\carte\go\src\todo\todo.exe"
+$Shortcut.Save()
--- todo.go
+++ todo.go
@@ -6,15 +6,46 @@ import (
 	"os"
 )
 
+func addNewTasks(reader *bufio.Reader){
+	var anotherTask bool = true
+	var text string = ""
+	var response string = ""
+	for anotherTask == true {
+
+		fmt.Println("Enter a task:")
+		text, _ = reader.ReadString('\n')
+		//Add the task to something
+		fmt.Println(text)
+		fmt.Println("Do you have another task to enter? (Y/N)")
+		response, _  = reader.ReadString('\n')
+		if response != "Y\r\n" {
+			anotherTask = false
+		//probably should add in another statement here to account for incorrect Y/N
+		} 
+	}
+}
+
+func openSticky(){
+	
+}
+
 func main() {
-
 	reader := bufio.NewReader(os.Stdin)
+	var isValidInput bool = false
+	var text string = ""
+	fmt.Println("Are there any new tasks to be done today? (Y/N):")
+	text, _ = reader.ReadString('\n')
+	for  isValidInput == false {
+		if text == "Y\r\n" {
+			isValidInput = true
+			addNewTasks(reader)
+		} else if text == "N\r\n" {
+			isValidInput = true
+		} else {
+			fmt.Println("Please enter either Y or N:")
+			text, _ = reader.ReadString('\n')
+		}
+	}
+	openSticky()
 
-	fmt.Println("TODO list:")
-	fmt.Println("1: View list")
-	fmt.Println("2: Enter New Task")
-	fmt.Println("3: Edit Task")
-	fmt.Println("4: Delete Task")
-	text, _ := reader.ReadString('\n')
-	fmt.Println(text)
 }
